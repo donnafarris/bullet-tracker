c92dc6c19d2c813bd721f46114574a77ea3c08bc
donnafarris <dlfarris97@gmail.com>
2020-11-23 11:39:18 -0500

    Formatting Update

--- bullet-tracker-api/server/app.js
+++ bullet-tracker-api/server/app.js
@@ -8,49 +8,46 @@ const db = require("./queries");
 const fetch = require("node-fetch"); //allows external API calls
 
 fetch("http://localhost:6001/users/1") //calls Ada's Account Management System
-    .then( //need to replace user id "1" with current session user ID
-        (res) => res.text())
-    .then(
-        (body) => console.log("Fetch Body:", body))
-    .catch(
-        (err) => console.error(err)
-    )
-;
+  .then(
+    //need to replace user id "1" with current session user ID
+    (res) => res.text()
+  )
+  .then((body) => console.log("Fetch Body:", body))
+  .catch((err) => console.error(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 app.use("/", (req, res, next) => {
-    //after each request, logs the path
-    console.log("Received request: " + req.originalUrl);
-    next(); // Passes control to the next middleware function. Otherwise, the request will be left hanging.
+  //after each request, logs the path
+  console.log("Received request: " + req.originalUrl);
+  next(); // Passes control to the next middleware function. Otherwise, the request will be left hanging.
 });
 
 app.on("connection", (req, res, next) => {
-    //sets default cookie for mocking purposes
-    let adminUser = { UID: "admin" };
-    let result;
-    if (adminUser.UID) {
-        result = { status: "success", message: "You are logged in." };
-        res.cookie("UID", adminUser.UID);
-        console.log("Cookies:", res.cookies.UID);
-    } else {
-        result = {
-            status: "failed",
-            message: "You have not logged in. Try /login.",
-        };
-        res.status(404);
-    }
-    res.json(result);
-    res.end();
+  //sets default cookie for mocking purposes
+  let adminUser = { UID: "admin" };
+  let result;
+  if (adminUser.UID) {
+    result = { status: "success", message: "You are logged in." };
+    res.cookie("UID", adminUser.UID);
+    console.log("Cookies:", res.cookies.UID);
+  } else {
+    result = {
+      status: "failed",
+      message: "You have not logged in. Try /login.",
+    };
+    res.status(404);
+  }
+  res.json(result);
+  res.end();
 });
-
 
 //edit/update bullets?
 
@@ -65,5 +62,5 @@ app.get("/search", db.searchBullets); //searches the narrative column for query,
 app.post("/new", db.newBullet); //inserts new bullets into bullets table
 
 app.listen(port, () =>
-    console.log(`Bullet Tracker Server listening at http://localhost:${port}`)
+  console.log(`Bullet Tracker Server listening at http://localhost:${port}`)
 );
